7ddba0e9f2b5d5e5b908d2f1eba41cc09ec0d8bb
NightLore <nathanlui2010@gmail.com>
2017-02-23 17:15:43 -0800

    Trying out a Generation Hierarchy
    template for the types of generation,
    Readded in an old version

---
+++ game/world/generations/Generation.java
@@ -0,0 +1,14 @@
+package game.world.generations;
+
+/**
+ *  Represents a type of generation for a single LargeTile/Chunk of the map
+ *
+ *  @author  Nathan M. Lui
+ *  @version May 3, 2016
+ *  @author  Assignment: OpenEnded
+ */
+public interface Generation
+{
+    public void generate();
+    
+}
---
+++ game/world/generations/SpawnGeneration.java
@@ -0,0 +1,24 @@
+package game.world.generations;
+
+/**
+ *  TODO Write a one-sentence summary of your class here.
+ *  TODO Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author  Nathan
+ *  @version May 3, 2016
+ *  @author  Assignment: OpenEnded
+ */
+public class SpawnGeneration extends GenerationAdapter
+{
+
+    /**
+     * 
+     */
+    public SpawnGeneration()
+    {
+        // TODO Auto-generated constructor stub
+
+    }
+
+}
